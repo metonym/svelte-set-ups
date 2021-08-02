a5da82e843e649998419e974dfa0c38862b6dbf2
Eric Liu <ericyl.us@gmail.com>
2021-08-02 07:29:24 -0700

    chore: upgrade deps

--- sveltekit/svelte.config.js
+++ sveltekit/svelte.config.js
@@ -1,8 +1,10 @@
+import adapter from "@sveltejs/adapter-node"
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
+    adapter: adapter()
   },
 };
 
