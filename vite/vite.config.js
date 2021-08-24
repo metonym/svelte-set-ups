4989762f56df136aacf90d03dc49c4b50fa73bc1
Eric Liu <ericyl.us@gmail.com>
2021-08-24 06:54:16 -0700

    chore: upgrade deps

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,9 +1,8 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [svelte()],
-    build: { minify: mode === "production" },
   };
 });
